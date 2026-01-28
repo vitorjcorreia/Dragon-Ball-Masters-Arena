aaa6f504bce263a8b609e45459b89f6334399a88
Vitor Correia <vitorjcorreia86@gmail.com>
2026-01-28 12:48:31 +0000

    fixed type according to deck building rules removed back property from cards that are not leaders

--- src/generate_card_assets.py
+++ src/generate_card_assets.py
@@ -9,6 +9,17 @@ ASSETS_PATH = "./assets"
 IMAGE_SOURCE_URL = "https://multi-deckplanet.us-southeast-1.linodeobjects.com/dbs_masters" 
 # https://multi-deckplanet.us-southeast-1.linodeobjects.com/dbs_masters/BT13-037.webp
 
+CARD_TYPES_MAPPING = {
+    "LEADER":   "Leader",
+    "BATTLE":   "Main_Deck",
+    "EXTRA":    "Main_Deck",
+    "UNISON":   "Main_Deck",
+    "Z-BATTLE": "Z_Deck",
+    "Z-EXTRA":  "Z_Deck",
+    "Z-UNISON": "Z_Deck",
+    "Z-LEADER": "Z_Deck"
+}
+
 def download_image(url: str, path: str):
     r = requests.get(url)
     if r.status_code == 200:
@@ -60,46 +71,49 @@ with open(input_file, 'r', encoding='utf-8') as f:
                 'face': {
                     'front': {
                         'name': card['card_name'],
-                        'type': card['card_type'],
+                        'type': CARD_TYPES_MAPPING[card['card_type']],
                         'cost': card['card_energy_cost'],
                         'power': card['card_power'],
+                        'Energy_cost': card['card_energy_cost'],
                         'image': img_url,
-                        'isHorizontal': card['card_type'] is 'Z-EXTRA'
-                    },
-                    'back': {
-                        'name': card['card_back_name'],
-                        'type': card['card_type'],
-                        'cost': card['card_energy_cost'],
-                        'power': card['card_back_power'],
-                        'image': f'{IMAGE_SOURCE_URL}/{card["img_link"]}_b.webp',
-                        'isHorizontal': False
-                    } if card['card_type'] == 'LEADER' else {}
+                        'isHorizontal': card['card_type'] == 'Z-EXTRA'
+                    }
                 },
-                'name': card['card_name'],
-                'type': card['card_type'],
-                'rarity': card['card_rarity'],
+                'Name': card['card_name'],
+                'Type': card['card_type'],
+                'Rarity': card['card_rarity'],
                 'image': img_url,
-                'power': card['card_power'],
-                'color': card['card_color'],
-                'energy_cost': card['card_energy_cost'],
-                'combo_cost': card['card_combo_cost'],
-                'combo_power': card['card_combo_power'],
-                'series': series,
-                'character': card['card_character'],
-                'era': card['card_era'],
-                'traits': card['card_traits'],
-                'banned': card['is_banned'],
-                'limited': card['is_limited'],
-                'limited_to': card['limited_to'],
-                'keywords': card['keywords'],
-                'z_energy_cost': card['z_energy_cost'],
-                'back_name': card['card_back_name'],
-                'back_power': card['card_back_power'],
-                'back_character': card['card_back_character'],
-                'back_era': card['card_back_era'],
-                'back_traits': card['card_back_traits'],
+                'Power': card['card_power'],
+                'Color': card['card_color'],
+                #'Energy_cost': card['card_energy_cost'],
+                'Combo_cost': card['card_combo_cost'],
+                'Combo_power': card['card_combo_power'],
+                'Series': series,
+                'Character': card['card_character'] if card['card_character'] is not None else [],
+                #'era': card['card_era'],
+                'Traits': card['card_traits'],
+                'Banned': card['is_banned'],
+                #'limited': card['is_limited'],
+                #'limited_to': card['limited_to'],
+                'Keywords': card['keywords'],
+                #'z_energy_cost': card['z_energy_cost'],
+                #'back_name': card['card_back_name'],
+                #'back_power': card['card_back_power'],
+                #'back_character': card['card_back_character'],
+                #'back_era': card['card_back_era'],
+                #'back_traits': card['card_back_traits'],
             }
         }
+
+        if card['card_type'] == 'LEADER':
+            card_entry[f'{card["card_number"]}']['face']['back'] = {
+                'name': card['card_back_name'],
+                'type': CARD_TYPES_MAPPING[card['card_type']],
+                'cost': card['card_energy_cost'],
+                'power': card['card_back_power'],
+                'image': f'{IMAGE_SOURCE_URL}/{card["img_link"]}_b.webp',
+                'isHorizontal': False
+            }
         cards_grouped_by_series.setdefault(series_norm, {}).update(card_entry)
 
 for series, cards in tqdm(cards_grouped_by_series.items(), desc="Generating card assets by series"):
